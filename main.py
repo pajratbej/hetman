7042bc3346d7f865cad3a899f782bb43c23adefd
pajratbej <originalemail001@gmail.com>
2019-07-19 14:59:43 +0200

    Deleted SELECT.py

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
 import asyncio as asyncio
-import random as r
 import discord
 from discord import Embed, Color, Colour
 from discord.ext import commands
-import SELECT
-import json
 import os
 import sys
-import datetime
+from datetime import datetime
 import traceback
 
 bot = commands.Bot(command_prefix="!")
 
+today = datetime.today()
 extensions = ["Cogs.Events","Cogs.Database","Cogs.School", "Cogs.Memes"]
-
+if today.day == 20 and today.month == 7:
+    extensions.append("Cogs.Birthday")
 
 if __name__=="__main__":
     for extension in extensions:
@@ -22,5 +21,5 @@ if __name__=="__main__":
         except Exception as e:
             print(f"Caught an exception {e}", file=sys.stderr)
             traceback.print_exc()
-    
+       
 bot.run(os.environ["SEC_KEY"])
