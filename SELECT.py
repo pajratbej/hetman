e8faf3c0aa5b5c6f812048919567cc086e36b0a5
Simon <simon@localhost.localdomain>
2019-07-19 14:53:45 +0200

    Delete SELECT.py

--- SELECT.py
+++
@@ -1,3 +0,0 @@
-insults = ["Chińskie bajki są dla pedałów. Lepiej idź sie pouczyć matematyki :)", "Weź wypierdalaj z tym syfem.", "Hhhhhhhhhhhhhhhhhhhhhhhhhh. TFU",
-           "Potraktowałbym to tak jak żydów. Spaliłbym"]
-co_zrobiles = ['co zrobiles', 'co zrobiłes', 'co zrobileś', 'co zrobiłeś']
