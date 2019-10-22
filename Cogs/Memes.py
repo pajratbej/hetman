be2c415024c6c6c2f5fc6ab0727454cff78670a9
Szymon <40179152+pajratbej@users.noreply.github.com>
2019-10-22 12:35:14 +0200

    Update Memes.py

--- Cogs/Memes.py
+++ Cogs/Memes.py
@@ -25,7 +25,7 @@ class Memes(commands.Cog, name="Meme i jojo"):
     @commands.command(pass_context=True)
     async def game(self, ctx):
         StaticMethods.setGame(ctx.message.content[6:])
-        await self.bot.change_presence(activity=discord.Game(name=StaticMethods.getGame())
+        await self.bot.change_presence(activity=discord.Game(name=StaticMethods.getGame()))
             
     @commands.command(pass_context=True)
     async def h(self, ctx):
